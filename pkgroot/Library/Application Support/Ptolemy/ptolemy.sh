7b26a1d4d69f62dbf55024539bad7f7a86355e5c
Per Olofsson <magervalp@fastmail.fm>
2020-11-18 22:02:31 +0100

    Imported project.

---
+++ pkgroot/Library/Application Support/Ptolemy/ptolemy.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+
+if [[ $( arch ) != arm64* ]]; then
+    echo "Not ARM"
+    exit 0
+fi
+
+if [[ -f "/System/Library/LaunchDaemons/com.apple.oahd.plist" ]]; then
+    echo "Rosetta launch daemon is already present"
+    exit 0
+fi
+
+echo "Triggering Rosetta install"
+/usr/sbin/softwareupdate --install-rosetta --agree-to-license
+
+
+exit 0
